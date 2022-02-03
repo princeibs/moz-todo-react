526c8d8455020c00446278bfb1235c1725fb2b57
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-02-03 16:46:56 +0100

    Completed: Editing, filtering, conditional rendering

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ import FilterButton from "./components/FilterButton";
 import Form from "./components/Form";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+    All: () => true,
+    Active: task => !task.completed,
+    Completed: task => task.completed,
+}
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
     const [tasks, setTasks] = useState(props.tasks);
+    const [filter, setFilter] = useState("Add");
+    const filterList = FILTER_NAMES.map(name => (
+        <FilterButton key={name} name={name} isPressed={name === filter} setFilter={setFilter} />
+    ));
     function toggleTaskCompleted(id) {
         const updatedTasks = tasks.map(task => {
             if (id === task.id) {
@@ -19,8 +30,19 @@ function App(props) {
         const updatedTasks = tasks.filter(task => id !== task.id)
         setTasks(updatedTasks);
     }
-    const taskList = tasks.map(task => (
-        <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} deleteTask={deleteTask} />
+    function editTask(id, newName) {
+        const editedTaskList = tasks.map(task => {
+            if (task.id == id) {
+                return {...task, name: newName}
+            }
+            return tasks;
+        });
+        setTasks(editedTaskList);
+    }
+    const taskList = tasks
+        .filter(FILTER_MAP[filter])
+        .map(task => (
+            <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} deleteTask={deleteTask} editTask={editTask} />
     ));
     const taskNoun = taskList.length > 1 ? "tasks" : "task";
     const headingText = `${taskList.length} ${taskNoun} remaining`;
@@ -34,9 +56,7 @@ function App(props) {
             <h1>TodoMatic</h1>
             <Form addTask={addTask} />
             <div className="filters btn-group stack-exception">
-                <FilterButton />
-                <FilterButton />
-                <FilterButton />
+                {filterList}
             </div>
             <h2 id="list-heading">
                 {headingText}
